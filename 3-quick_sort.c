f7d46b97f0c0d56ef99201e842119d0795122e4c
internashionalist <taylor.thames@atlasschool.com>
2024-08-02 06:27:56 -0500

    3.0 - base

---
+++ 3-quick_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts an array of integers in ascending order
+ * using quick sort algorithm, printing the array after each swap
+ * @array: array of integers
+ * @size: size of the array
+ *
+ * Return: void
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	
+}
